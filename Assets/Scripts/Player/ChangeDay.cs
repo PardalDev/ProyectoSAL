36d2a1d32e6362b22dc2f5f68426c05ab5d4bcf6
Pardal Developers <PardalDevelopers@gmail.com>
2021-05-03 05:09:54 -0300

    AddFirstScene
    Se agrega la 1er escena de pantano

---
+++ Assets/Scripts/Player/ChangeDay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ChangeDay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject luz;
+    private Vector3 lightRotation;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+            Debug.Log("Rorating");
+
+            lightRotation = new Vector3(10, 0, 0);
+            luz.transform.eulerAngles = lightRotation;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerLocal.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PlayerLocal : MonoBehaviour
+{
+    private Vector2 moveAmount;
+    public int speed;
+    protected Joystick joystick;
+    protected Rigidbody2D rb;
+
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (!isLocalPlayer)
+        //   return;
+
+        Vector2 moveImput = new Vector2(Input.GetAxisRaw("Horizontal") + joystick.Horizontal, Input.GetAxisRaw("Vertical") + joystick.Vertical);
+        moveAmount = moveImput.normalized * speed;
+
+        //Chequeo si se esta moviendo
+        if (moveImput != Vector2.zero)
+        {
+
+            if (Input.GetKeyDown("left shift"))
+            {
+                //anim.SetBool("isWalking", true);
+                //anim.SetBool("is_running", true);
+            }
+            else
+            {
+                //anim.SetBool("isWalking", true);
+                //anim.SetBool("is_running", true);
+            }
+
+        }
+        else
+        {
+            //anim.SetBool("isWalking", false);
+            //anim.SetBool("is_running", false);
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+            rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+            
+    }
+}
